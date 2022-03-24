e1816b08e22a5a19e8bdff0f896ca2e9c75942ab
Zeb Piasecki <zeb@zebulon.dev>
2022-03-24 17:17:22 -0400

    refactor: error handling and whatnot

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,54 @@
+mod error;
+
 use std::path::PathBuf;
 use std::{fs, path::Path};
 
 use clap::Command;
 use directories::BaseDirs;
+use error::GeneralError;
 use rusqlite::Connection;
 
-#[derive(Debug)]
-struct Location {
+#[derive(Debug, Clone)]
+pub struct Location {
     location: String,
 }
 
-fn mark_path(
-    connection: Connection,
-    args: (Option<&str>, Option<&str>),
-) -> Result<(), Box<dyn std::error::Error>> {
-    match args {
-        (Some(path), Some(tags)) => {
-            let dir = PathBuf::from(path.trim());
-            if !Path::new(&dir).exists() {
-                panic!("Path does not exist");
-            }
-
-            let absolute_path = fs::canonicalize(&dir)
-                .unwrap()
-                .to_string_lossy()
-                .to_string();
-            for tag in tags.split(",") {
-                connection
-                    .execute(
-                        "INSERT OR IGNORE INTO tagged (location, tag) VALUES (?1, ?2)",
-                        &[&absolute_path, &tag.trim().into()],
-                    )
-                    .unwrap();
-            }
-            Ok(())
-        }
-        _ => panic!("Bruh moment"),
+fn mark_path(connection: Connection, path: &str, tags: &str) -> Result<(), GeneralError> {
+    let dir = PathBuf::from(path.trim());
+    if !Path::new(&dir).exists() {
+        panic!("Path does not exist");
     }
+
+    let absolute_path = fs::canonicalize(&dir)?.to_string_lossy().to_string();
+
+    for tag in tags.split(",") {
+        connection.execute(
+            "INSERT OR IGNORE INTO tagged (location, tag) VALUES (?1, ?2)",
+            &[&absolute_path, &tag.trim().into()],
+        )?;
+    }
+    Ok(())
 }
 
-fn find_path(connection: Connection, tags: Option<&str>) -> Result<(), Box<dyn std::error::Error>> {
-    match tags {
-        Some(tags) => {
-            for tag in tags.split(",") {
-                let mut statement =
-                    connection.prepare("SELECT location FROM tagged WHERE tag LIKE ?")?;
+fn find_path(connection: Connection, tags: &str) -> Result<(), rusqlite::Error> {
+    for tag in tags.split(",") {
+        let mut statement = connection.prepare("SELECT location FROM tagged WHERE tag LIKE ?")?;
 
-                let paths = statement.query_map(&[&tag.trim()], |row| {
-                    Ok(Location {
-                        location: row.get(0)?,
-                    })
-                })?;
+        let paths = statement.query_map(&[&tag.trim()], |row| {
+            Ok(Location {
+                location: row.get(0)?,
+            })
+        })?;
 
-                for path in paths {
-                    println!("{}", path.unwrap().location);
-                }
-            }
-            Ok(())
+        for path in paths {
+            println!("{}", path?.location);
         }
-        _ => panic!("Bruh moment"),
     }
+
+    Ok(())
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), GeneralError> {
     let mut dir = PathBuf::new();
     if let Some(base_dirs) = BaseDirs::new() {
         dir.push(base_dirs.config_dir());
@@ -71,8 +57,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     let path = Path::new(&dir);
+
     if !path.exists() {
-        fs::create_dir_all(&dir.parent().unwrap()).unwrap();
+        if let Some(parent) = dir.parent() {
+            fs::create_dir_all(parent)?;
+        }
     }
 
     let connection = Connection::open(dir)?;
@@ -104,12 +93,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .get_matches();
 
-    return match matches.subcommand() {
-        Some(("mark", sub_matches)) => mark_path(
-            connection,
-            (sub_matches.value_of("PATH"), sub_matches.value_of("TAGS")),
-        ),
-        Some(("find", sub_matches)) => find_path(connection, sub_matches.value_of("TAGS")),
+    match matches.subcommand() {
+        Some(("mark", sub_matches)) => {
+            let path: String = sub_matches.value_of_t_or_exit("PATH");
+            let tags: String = sub_matches.value_of_t_or_exit("TAGS");
+            mark_path(connection, &path, &tags)?;
+        }
+        Some(("find", sub_matches)) => {
+            let tags: String = sub_matches.value_of_t_or_exit("TAGS");
+            find_path(connection, &tags)?;
+        }
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     };
+
+    Ok(())
 }
